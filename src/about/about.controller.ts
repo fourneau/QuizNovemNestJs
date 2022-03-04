925c80c7c87bb98605ecf7c41fde6e19e6514b00
fourneau <68759271+fourneau@users.noreply.github.com>
2022-03-04 16:23:27 +0100

    Méthodes About

--- src/about/about.controller.ts
+++ src/about/about.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('about')
-export class AboutController {}
+export class AboutController {
+@Get()
+getAbout(){
+    console.log('récupérer')
+    return 'Liste';
+}
+@Post()
+addAbout(){
+    console.log('ajouter')
+    return 'Ajout About';
+}
+@Delete()
+deleteAbout(){
+    console.log('supprimer')
+    return 'Delete About';
+}
+@Put()
+modifierAbout(){
+    console.log('modifier About')
+    return 'Update About';
+}
+}
