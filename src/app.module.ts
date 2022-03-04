82ca42801f229d906e9b5b633fdc2e732eab4d7c
fourneau <68759271+fourneau@users.noreply.github.com>
2022-03-04 11:19:18 +0100

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ComponentsModule } from './-components/-components.module';
+import { ContentModule } from './-content/-content.module';
+import { HelpersModule } from './-helpers/-helpers.module';
+import { ModelsModule } from './-models/-models.module';
+import { ServicesModule } from './-services/-services.module';
+import { AboutModule } from './about/about.module';
+import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { SigninModule } from './signin/signin.module';
+import { WelcomeModule } from './welcome/welcome.module';
 
 @Module({
-  imports: [],
+  imports: [ComponentsModule, ContentModule, HelpersModule, ModelsModule, ServicesModule, AboutModule, HomeModule, LoginModule, RegisterModule, SigninModule, WelcomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
